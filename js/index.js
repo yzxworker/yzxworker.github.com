b485dd19ca8c2cacedb02b3fdbeb69f15c4aec01
yzxworker <yzxisaworker@sina.com>
2016-07-15 23:25:09 +0800

    log

--- js/index.js
+++ js/index.js
@@ -26,13 +26,19 @@ $(document).ready(function(){
 	
 	$('#headnav li').on('click',function(){
 		$('#zynavbar').animate({left:$(this).position().left,width:$(this).outerWidth()});
-		// $('#dowebok').css({transform: 'translate3d(0px, -'+$(this).index()*$('#dowebok').outerHeight()+'px, 0px)'});
-		// console.log($(this).index()*$('#dowebok').outerWidth());
+	
 	});
 	$('#headnav-min li').on('click',function(){
 		$('#headnav-min li a').css({background:'#ccc'});
 		$(this).find('a').css({background:'#21d4b6'});
 
+	});
+
+	// 侧边栏
+	$('.qqandwc li').hover(function(){
+		$('.qqxq li').eq($(this).index()).animate({left:-108,opacity:1});
+	},function(){
+		$('.qqxq li').eq($(this).index()).animate({left:0,opacity:0});
 	});
 
 
@@ -188,17 +194,92 @@ $(document).ready(function(){
 			    mapName: 'china',
 			    mapWidth: oP7width,
 			    mapHeight: oP7height,
-			    hoverCallback: function(stateData, obj){
+			    hoverCallback: function(stateData, obj){//hoverCallback
 			        $('#ClickCallback').html('hover:'+obj.name);
-			        console.log(obj.name);
-			        if(obj.name=='陕西'){
-			        	$('.contact-con .region-search .region-show').css('display','block');
-			        }else{
-			        	$('.contact-con .region-search .region-show').css('display','none');	
-			        }
+			        $.ajax({
+			        	type:'post',
+			        	url:'http://113.139.98.82/web/index.php?g=user&m=Contact',
+			        	data:{
+			        		code:obj.name
+
+			        	},
+			        	success:function(data) {    
+			        		
+					        if(data){
+					        	$('.contact-con .region-search .region-show').css('display','block');
+					            oP7region_show[0].innerHTML='';
+					            alert(data); 
+					            var arr=eval('('+data+')');
+					            for (var i = 0; i < arr.length; i++) {
+					            	var oP=document.createElement('p');
+					            	oP.innerHTML='<span>地址：'+arr[i].province+arr[i].city+arr[i].address+'</span><i>电话：'+arr[i].phone+'</i>';
+					            	oP7region_show[0].appendChild(oP);
+					            }
+			   					// if(obj.name){
+						     //    	$('.contact-con .region-search .region-show').css('display','block');
+						     //    }else{
+						     //    	$('.contact-con .region-search .region-show').css('display','none');	
+						     //    }
+							                
+					        }else{    
+					            alert(data.msg); 
+					        $('.contact-con .region-search .region-show').css('display','none');
+					        }    
+					    },    
+					    error : function() {       
+					          alert("网络异常！");    
+					    }    
+
+			        })
 			    }
 		});
+
 	};
+
+	var oP7search_text=$('.region-search .search input');
+	var oP7search_btn=$('.region-search .search a');
+
+
+	$('.region-search .search a').on('click',function(){
+		
+		var str=$('.region-search .search input').val();
+
+		var re=/[^\u4e00-\u9fa5]+/;
+		if(re.test(str)) {
+			alert('请输入省份');
+		}else{
+
+		 $.ajax({
+		 		type:'post',
+	        	url:'http://113.139.98.82/web/index.php?g=user&m=Contact',
+	        	data:{
+	        		code:str
+	        	},
+	        	success:function(data) {    
+	        		
+			        if(data){
+			        	$('.contact-con .region-search .region-show').css('display','block');
+			            oP7region_show[0].innerHTML='';
+			            alert(data); 
+			            var arr=eval('('+data+')');
+			            for (var i = 0; i < arr.length; i++) {
+			            	var oP=document.createElement('p');
+			            	oP.innerHTML='<span>地址：'+arr[i].province+arr[i].city+arr[i].address+'</span><i>电话：'+arr[i].phone+'</i>';
+			            	oP7region_show[0].appendChild(oP);
+			            }
+					                
+			        }else{    
+			            alert(data.msg);         
+			        }    
+			    },    
+			    error : function() {       
+			          alert("网络异常！");    
+			    }    
+
+	        })
+		 }
+
+	})
 
 
 	// zyl
@@ -301,7 +382,7 @@ $(document).ready(function(){
 	$('#loginbtn').on('click',function(){
 		$.ajax({
 			url:loginurl,
-
+			type:'post',
 			data:{
 				g:$('#username').val(),
 				m:$('#password').val()
@@ -309,6 +390,14 @@ $(document).ready(function(){
 			success:function(str){
 				$('#uselogin').css({display:'none'});
 				$('#loginbtn').css({display:'none'});
+				$('.p5-jl').css({display:'block'});
+				$('.p5-tit3').removeClass('hide');
+				$('.p5-tit3').addClass('show');
+				$('.p5-tit1').removeClass('show');
+				$('.p5-tit1').addClass('hide');
+				$('.p5-tit').removeClass('show');
+				$('.p5-tit').addClass('hide');
+				$('.p5-tit3').html('你好！');
 			},
 			error:function(err){
 				alert(err.msg);
@@ -334,6 +423,7 @@ $(document).ready(function(){
 		},1000);
 		$.ajax({
 			url:msgurl,
+			type:'post',
 			data:{
 				'number':$('#msg').val()
 			},
@@ -352,7 +442,7 @@ $(document).ready(function(){
 		$('#sub').css({display:'none'});
 	});
 
-	var suburl='http://113.139.98.82/web/index.php?g=user&m=sendsms&a=reg';
+	var submiturl='http://113.139.98.82/web/index.php?g=user&m=sendsms&a=reg';
 	$('#button1').click(function(){
 			var sex='';
 			if($('#sex').checked){
@@ -361,7 +451,8 @@ $(document).ready(function(){
 				sex='女';
 			}
 			$.ajax({
-				url:suburl,
+				url:submiturl,
+				type:'post',
 				data:{
 					'user':$('#user').val(),
 					'pwd':$('#name').val(),
@@ -373,11 +464,60 @@ $(document).ready(function(){
 					$('#sub').css({display:'none'});
 				},
 				error:function(err){
-					alert('err');
+					$('#subtips').html(err.msg);
 				}
 			});
 		
 	});
+
+
+	// 提交订单
+	var subUrl='http://113.139.98.82/web/index.php?g=user&m=Integral';
+	$('.p3-introduce-img li').click(function(){
+		$('#submit').css('display','block');
+	});
+	$('#close-sub').click(function(){
+		$('#submit').css('display','none');
+	});
+	
+	$('#sub-btn1').click(function(){
+		$.ajax({
+			type:"post",
+			url:subUrl,
+			data:{
+				'Numbering':$('#number').val(),
+				'name':$('#sub-name').val(),
+				'address':$('#address').val(),
+				'zipcode':$('#zipcode').val(),
+				'phone':$('#sub-tel').val(),
+				'Remark':$('#area').val()
+			},
+			success:function(str){
+				$('#submit').css({display:'none'});
+			},
+			error:function(err){
+				alert('err'+err);
+			}
+		});
+	});
+
+	//兑换
+	var Surl='http://113.139.98.82/web/index.php?g=user&m=Integral';
+	$('#p5-text2').click(function(){			
+		$.ajax({
+			url:Surl,
+			data:{
+				'key':$('#p5-text1').val()
+			},
+			success:function(str){
+				
+			},
+			error:function(err){
+				alert('err');
+			}
+		});
+	});
+
 	
 });
 
